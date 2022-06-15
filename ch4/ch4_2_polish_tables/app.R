2ba54415a78d3a9730365862b9d785d7e818ad76
Johnny Wharton <johnnyjw@gmail.com>
2022-06-15 07:45:50 +0100

    ch4 resume

---
+++ ch4/ch4_2_polish_tables/app.R
@@ -0,0 +1,56 @@
+library(shiny)
+library(vroom)
+library(tidyverse)
+
+prod_codes <- setNames(products$prod_code, products$title)
+
+count_top <- function(df, var, n = 5) {
+  df %>% 
+    mutate({{ var }} := fct_lump(fct_infreq({{ var}}), n = n)) %>% 
+    group_by({{ var }}) %>% 
+    summarise(n = as.integer(sum(weight)))
+    
+}
+
+ui <- fluidPage(
+  fluidRow(
+    column(6,
+           selectInput("code", "Product", choices = prod_codes)
+           )
+  ),
+  fluidRow(
+    column(4, tableOutput("diag")),
+    column(4, tableOutput("body_part")),
+    column(4, tableOutput("location"))
+  ),
+  fluidRow(
+    column(12, plotOutput("age_sex"))
+  )
+)
+
+server <- function(input, output, server) {
+  selected <- reactive(injuries %>% filter(prod_code == input$code))
+  
+  output$diag <- renderTable(count_top(selected(), diag), width = "100%")
+  
+  output$body_part <- renderTable(count_top(selected(), body_part), width = "100%")
+  
+  output$location <- renderTable(count_top(selected(), location), width = "100%")
+  
+  summary <- reactive({
+    selected() %>% 
+      count(age, sex, wt = weight) %>% 
+      left_join(population, by = c("age", "sex")) %>% 
+      mutate(rate = n / population * 1e4)
+  })
+  
+  output$age_sex <- renderPlot({
+    summary() %>% 
+      ggplot(aes(age, n, colour = sex)) +
+      geom_line() +
+      labs(y = "Estimated number of injuries")
+  }, res = 96)
+
+}
+
+shinyApp(ui, server)
--- ch4/get_some_data.R
+++ ch4/get_some_data.R
@@ -3,15 +3,15 @@ library(vroom)
 library(tidyverse)
 
 
-dir.create("neiss")
+#dir.create("neiss")
 #> Warning in dir.create("neiss"): 'neiss' already exists
-download <- function(name) {
-  url <- "https://github.com/hadley/mastering-shiny/raw/master/neiss/"
-  download.file(paste0(url, name), paste0("neiss/", name), quiet = TRUE)
-}
-download("injuries.tsv.gz")
-download("population.tsv")
-download("products.tsv")
+#download <- function(name) {
+#  url <- "https://github.com/hadley/mastering-shiny/raw/master/neiss/"
+#  download.file(paste0(url, name), paste0("neiss/", name), quiet = TRUE)
+#}
+#download("injuries.tsv.gz")
+#download("population.tsv")
+#download("products.tsv")
 
 injuries <- vroom::vroom("neiss/injuries.tsv.gz")
 injuries
@@ -29,3 +29,30 @@ selected %>% count(location, wt = weight, sort = TRUE)
 selected %>% count(body_part, wt = weight, sort = TRUE)
 
 selected %>% count(diag, wt = weight, sort = TRUE)
+
+summary <- selected %>% 
+  count(age, sex, wt = weight)
+summary
+
+summary %>% 
+  ggplot(aes(age, n, color = sex)) +
+  geom_line() +
+  labs(y = "Estimated number of injuries")
+
+# rate per 10000
+summary <- selected %>% 
+  count(age, sex, wt = weight)%>% 
+  left_join(population, by = c("age", "sex")) %>% 
+  mutate(rate = n / population * 1e4)
+
+summary
+
+summary %>% 
+  ggplot(aes(age, rate, color = sex)) +
+  geom_line() +
+  labs(y = "Inguries per 10,000 people")
+
+# look at narratives
+selected %>% 
+  sample_n(10) %>% 
+  pull(narrative)
